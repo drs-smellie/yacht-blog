0d774a6a43f47103badffae1443e713c871fdcf8
Melanie <makes.melly@gmail.com>
2014-02-10 16:46:21 +0100

    Additional updates for authorisation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,7 @@ class CommentsController < ApplicationController
 		@new_comment = Comment.new
 		redirect_to post_path(@post)
 
+		authorize! :create, @comment, message: "You need to be signed up to do that."
 		if @comment.save
 			flash[:notice] = "Comment was saved!"
 		else
@@ -20,7 +21,13 @@ class CommentsController < ApplicationController
 	def destroy
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.find(params[:id])
-		@comment.destroy
+
+		authorize! :destroy, @comment, message: "You need to own the comment to delete it."
+		if @comment.destroy
+			flash[:notice] = "Comment was removed."
+		else
+			flash[:error] = "There was an error deleting the comment. Please try again."
+		end
 		redirect_to post_path(@post)
 	end
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,8 +45,13 @@ class PostsController < ApplicationController
 
 	def destroy
 		@post = Post.find(params[:id])
-		@post.destroy
-
+		authorize! :destroy, @post, message: "You need to be an admin to do this!"
+		
+		if @post.destroy
+			flash[:notice] = "Post was removed!"
+		else
+			flash[:error] = "There was an error deleting the comment. Please try again."
+		end
 		redirect_to posts_path
 	end
 

1c35ab1d43dd9f109362fce6e5580ee6a4229871
Melanie <makes.melly@gmail.com>
2014-02-12 12:57:11 +0100

    Added validations for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments, dependent: :destroy
-	validates 	:title, presence: true,
-					length: { minimum: 3 }
+	validates 	:title, length: { minimum: 3 }, presence: true
+	validates :text, length: { minimum: 10 }, presence: true
+	validates :user, presence: true
+
 end

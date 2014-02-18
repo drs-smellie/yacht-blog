75118ce8cc3d1f4b1e3a461052298c94b78438cc
Melanie <makes.melly@gmail.com>
2014-02-18 13:26:53 +0100

    added edit and show pages for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,25 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def show
+		@user = User.find(params[:id])
+		@posts = @user.posts
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			render :edit, notice: "User was saved successfully!"
+		else
+			flash[:error] = "Error saving user. Please try again."
+			render :edit
+		end 
+	end
+
 	def create
 		@user = User.new(user_params)
 		if @user.save
